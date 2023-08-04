5bf581e8ac6e20117965ca9f91095ae486c5a38b
maksim <maxim.bezmen@gmail.com>
2023-08-04 23:44:13 +0300

    Add s3

--- src/main/java/com/aws/mb/awsmb/Controller/S3Controller.java
+++ src/main/java/com/aws/mb/awsmb/Controller/S3Controller.java
@@ -4,7 +4,6 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
-import com.amazonaws.services.s3.model.S3Object;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
